53702d3b2c139d502a75b930876649b5547842ae
sashakhandus <sashakhandus@gmail.com>
2018-04-17 15:06:59 +0300

    17.04.2018 15.07

---
+++ js/module01/js/index.js
@@ -0,0 +1,71 @@
+let numbersTaba = 6;
+let numbersSharm = 15;
+let numbersHurgada = 25;
+
+let numbersUser = prompt('Введите необходимое число мест');
+
+console.log(numbersUser);
+console.log(typeof numbersUser);
+
+
+if (typeof Number.parseInt(numbersUser) === 'number' && !Number.isNaN(numbersUser) && (Number.parseInt(numbersUser) >= 0)) {
+    if (numbersUser <= numbersTaba) {
+       let answerUser = confirm ('Есть места в группе Taba. Вы согласны быть в этой группе?');
+       if (answerUser) {
+            numbersTaba = numbersTaba - numbersUser;
+            alert('Приятного путешествия в группе Taba!');
+       } else {
+           if (numbersUser <= numbersSharm) {
+                let answerUser = confirm ('Есть места в группе Sharm. Вы согласны быть в этой группе?');
+                if (answerUser) {
+                    numbersSharm = numbersSharm - numbersUser;
+                    alert('Приятного путешествия в группе Sharm!');
+                } else {
+                    if (numbersUser <= numbersHurgada) {
+                        let answerUser = confirm ('Есть места в группе Hurgada. Вы согласны быть в этой группе?');
+                        if (answerUser) {
+                            numbersHurgada = numbersHurgada - numbersUser;
+                            alert('Приятного путешествия в группе Hurgada!');
+                        } else {
+                            alert('Изините, мест нет.');
+                        }
+                   }
+               }
+           }
+       }
+    } else {
+        if (numbersUser <= numbersSharm) {
+            let answerUser = confirm ('Есть места в группе Sharm. Вы согласны быть в этой группе?');
+            if (answerUser) {
+                numbersSharm = numbersSharm - numbersUser;
+                alert('Приятного путешествия в группе Sharm!');
+        } else {
+            if (numbersUser <= numbersHurgada) {
+                let answerUser = confirm ('Есть места в группе Hurgada. Вы согласны быть в этой группе?');
+                if (answerUser) {
+                    numbersHurgada = numbersHurgada - numbersUser;
+                    alert('Приятного путешествия в группе Hurgada!');
+             } else {
+                 alert('Изините, мест нет.');
+                }
+            }
+        }
+    
+   } else {
+       if (numbersUser <= numbersHurgada) {
+        let answerUser = confirm ('Есть места в группе Hurgada. Вы согласны быть в этой группе?');
+        if (answerUser) {
+            numbersHurgada = numbersHurgada - numbersUser;
+            alert('Приятного путешествия в группе Hurgada!');
+        } else {
+            alert('Изините, мест нет.');
+            }
+        }
+        else {
+            alert('Изините, мест нет.');
+        }
+    }
+}
+} else {
+    alert('Ошибка ввода.');
+}
