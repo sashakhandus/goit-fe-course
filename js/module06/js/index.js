2b0a47167b4ebf8ff2065dd70770ebdc55085c87
sashakhandus <sashakhandus@gmail.com>
2018-06-05 10:48:17 +0300

    module-06 05.06.2018

--- js/module06/js/index.js
+++ js/module06/js/index.js
@@ -5,6 +5,73 @@ class Hamburger {
         this.stuffing = stuffing;
         this.toppings = [];
     }
+
+    addTopping (topping) {
+        this.toppings.push(topping);
+    };
+        
+    removeTopping (topping) {
+        this.toppings.splice(this.toppings.indexOf(topping), 1);
+    };
+        
+    getSize () {
+        return this.size;
+    };
+    
+    getSizePrice () {
+        return (Hamburger.SIZES[hamburger.getSize()]['price']);
+    };
+    
+    getSizeCalories () {
+        return (Hamburger.SIZES[hamburger.getSize()]['calories']);
+    };
+    
+    getStuffing () {
+        return this.stuffing;
+    };
+    
+    getStuffingPrice () {
+        return (Hamburger.STUFFINGS[hamburger.getStuffing()]['price']);
+    };
+    
+    getStuffingCalories () {
+        return (Hamburger.STUFFINGS[hamburger.getStuffing()]['calories']);
+    };
+    
+    getToppings () {
+        return this.toppings;
+    };
+    
+    getToppingsPrice () {
+        let toppingPrice = 0;
+        for (let index in this.toppings) {
+            toppingPrice = toppingPrice + Hamburger.TOPPINGS[this.toppings[index]]['price'];
+        };
+        return toppingPrice;
+    };
+    
+    getToppingsCalories () {
+        let toppingCalories = 0;
+        for (let index in this.toppings) {
+            toppingCalories = toppingCalories + Hamburger.TOPPINGS[this.toppings[index]]['calories'];
+        };
+        return toppingCalories;
+    };
+    
+    calculatePrice () {
+        if (hamburger.getToppings().length > 0) {
+            return (hamburger.getSizePrice() + hamburger.getStuffingPrice() + hamburger.getToppingsPrice());
+        } else {
+            return (hamburger.getSizePrice() + hamburger.getStuffingPrice());
+        };
+    };
+    
+    calculateCalories () {
+        if (hamburger.getToppings().length > 0) {
+            return hamburger.getSizeCalories() + hamburger.getStuffingCalories() + hamburger.getToppingsCalories();
+        }
+        return hamburger.getSizeCalories() + hamburger.getStuffingCalories();
+    };
 }
 
     Hamburger.SIZE_SMALL = 'SIZE_SMALL';
@@ -55,78 +122,14 @@ class Hamburger {
         },
     };
 
-Hamburger.prototype.addTopping = function (topping) {
-    this.toppings.push(topping);
-};
-    
-Hamburger.prototype.removeTopping = function (topping) {
-    this.toppings.splice(this.toppings.indexOf(topping), 1);
-};
-    
-Hamburger.prototype.getSize = function() {
-    return this.size;
-};
-
-Hamburger.prototype.getSizePrice = function() {
-    return (Hamburger.SIZES[hamburger.getSize()]['price']);
-};
-
-Hamburger.prototype.getSizeCalories = function() {
-    return (Hamburger.SIZES[hamburger.getSize()]['calories']);
-};
-
-Hamburger.prototype.getStuffing = function() {
-    return this.stuffing;
-};
-
-Hamburger.prototype.getStuffingPrice = function() {
-    return (Hamburger.STUFFINGS[hamburger.getStuffing()]['price']);
-};
-
-Hamburger.prototype.getStuffingCalories = function() {
-    return (Hamburger.STUFFINGS[hamburger.getStuffing()]['calories']);
-};
-
-Hamburger.prototype.getToppings = function() {
-    return this.toppings;
-};
-
-Hamburger.prototype.getToppingsPrice = function() {
-    let toppingPrice = 0;
-    for (index in this.toppings) {
-        toppingPrice = toppingPrice + Hamburger.TOPPINGS[this.toppings[index]]['price'];
-    };
-    return toppingPrice;
-};
-
-Hamburger.prototype.getToppingsCalories = function() {
-    let toppingCalories = 0;
-    for (index in this.toppings) {
-        toppingCalories = toppingCalories + Hamburger.TOPPINGS[this.toppings[index]]['calories'];
-    };
-    return toppingCalories;
-};
-
-Hamburger.prototype.calculatePrice = function () {
-    if (hamburger.getToppings().length > 0) {
-        return (hamburger.getSizePrice() + hamburger.getStuffingPrice() + hamburger.getToppingsPrice());
-    } else {
-        return (hamburger.getSizePrice() + hamburger.getStuffingPrice());
-    };
-};
-
-Hamburger.prototype.calculateCalories = function () {
-    if (hamburger.getToppings().length > 0) {
-        return hamburger.getSizeCalories() + hamburger.getStuffingCalories() + hamburger.getToppingsCalories();
-    }
-    return hamburger.getSizeCalories() + hamburger.getStuffingCalories();
-};
 
 const hamburger = new Hamburger(
     Hamburger.SIZE_SMALL, 
     Hamburger.STUFFING_CHEESE,
 );
 
+console.log("Calories: ", hamburger.calculateCalories());
+console.log("Price: ", hamburger.calculatePrice());
 
 hamburger.addTopping(Hamburger.TOPPING_SPICE);
 
@@ -136,8 +139,14 @@ console.log("Price: ", hamburger.calculatePrice());
 hamburger.addTopping(Hamburger.TOPPING_SAUCE);
 console.log("Price with sauce: ", hamburger.calculatePrice());
 
+console.log("Calories: ", hamburger.calculateCalories());
+console.log("Price: ", hamburger.calculatePrice());
+
 console.log("Is hamburger large: ", hamburger.getSize() === Hamburger.SIZE_LARGE);
 
 hamburger.removeTopping(Hamburger.TOPPING_SAUCE);
+
+console.log("Calories: ", hamburger.calculateCalories());
+console.log("Price: ", hamburger.calculatePrice());
 
 console.log("Hamburger has %d toppings", hamburger.getToppings().length); 
