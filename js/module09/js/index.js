25d21cd8e41931d1fcc040b98178a5de50054dc8
sashakhandus <sashakhandus@gmail.com>
2018-06-22 23:15:22 +0300

    module-09

--- js/module09/js/index.js
+++ js/module09/js/index.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const btnStart = document.querySelector('.js-start');
-    const btnPause = document.querySelector('.js-pause');
     const btnStop = document.querySelector('.js-reset'); 
-    const btnLap = document.querySelector('.js-lap'); 
     const timeFace = document.querySelector('.js-time');
     const ul = document.querySelector('.js-laps');
     let laps = [];
@@ -21,10 +19,10 @@ document.addEventListener('DOMContentLoaded', () => {
             this.addLap = addLap;
         }
 
-        start () {
+        onclick () {
             if (!this.isActive) {
-                this.isPause = false;
                 this.isActive = true;
+                this.isPause = false;
                 this.startTime = Date.now();
 
                 this.timerId = setInterval(() => {
@@ -39,70 +37,61 @@ document.addEventListener('DOMContentLoaded', () => {
 
                     this.onTick({min, sec, ms});
                 }, 200);
-            } 
-        }
 
-        pause () {
-            console.log("Pause");
-            this.isPause = true;
-            this.pauseTime = this.deltaTime;
+                btnStart.textContent = 'Pause';
+                btnStop.textContent = 'Reset';
 
-            const time = new Date(this.pauseTime);
-            const min = time.getMinutes();
-            const sec = time.getSeconds();
-            const ms = Number.parseInt(time.getMilliseconds() / 100);
-
-            this.lapTime = ({ min, sec, ms }) ;
-
-            this.isActive = false;
-            clearInterval(this.timerId);
-            this.startTime = null;
-            this.deltaTime = 0;
-            this.onTick({min, sec, ms});
-             
-            btnStart.textContent = 'Contine';       
+            } else {
+                this.isPause = true;
+                this.pauseTime = this.deltaTime;
+    
+                const time = new Date(this.pauseTime);
+                const min = time.getMinutes();
+                const sec = time.getSeconds();
+                const ms = Number.parseInt(time.getMilliseconds() / 100);
+    
+                this.lapTime = ({ min, sec, ms }) ;
+    
+                this.isActive = false;
+                clearInterval(this.timerId);
+                this.startTime = null;
+                this.deltaTime = 0;
+                this.onTick({min, sec, ms});
+                 
+                btnStart.textContent = 'Contine'; 
+                btnStop.textContent = 'Lap';
+            }
         }
 
         stop () {
-            this.isPause = false;
-            this.isActive = false;
-            clearInterval(this.timerId);
-            this.timerId = null;
-            this.startTime = null;
-            this.deltaTime = 0;
-            this.onTick({ min: 0, sec: 0, ms: 0 });
+            if (!this.isPause) {
+                this.isActive = false;
+                clearInterval(this.timerId);
+                this.timerId = null;
+                this.startTime = null;
+                this.deltaTime = 0;
+                this.onTick({ min: 0, sec: 0, ms: 0 });
 
-            btnStart.textContent = 'Start';
+                btnStart.textContent = 'Start';
 
-            for (let i = 0; i < laps.length; i = i + 1) {
-                clearLaps();
-            }
-
-            laps = [];
+                for (let i = 0; i < laps.length; i = i + 1) {
+                    clearLaps();
+                }
+                laps = [];
+            } else {
+                this.addLap(this.lapTime); 
+            }   
         }
 
-        lap () {
-            if (this.isPause) {
-                const min = this.lapTime.min;
-                const sec = this.lapTime.sec;
-                const ms = this.lapTime.ms;
-                console.log(`${min}:${sec}.${ms}`);
-                this.addLap({ min, sec, ms });
-                this.isPause = false;
-            }
-        }
-
-        }
+    }
 
     const timer = new Timer({
         onTick: updateClockface,
         addLap: addLiToUl,
     });
 
-    btnStart.addEventListener('click', timer.start.bind(timer));
-    btnPause.addEventListener('click', timer.pause.bind(timer));
+    btnStart.addEventListener('click', timer.onclick.bind(timer));  
     btnStop.addEventListener('click', timer.stop.bind(timer));
-    btnLap.addEventListener('click', timer.lap.bind(timer));
 
     updateClockface({ min: 0, sec: 0, ms: 0 });
 
@@ -113,21 +102,25 @@ document.addEventListener('DOMContentLoaded', () => {
         if (sec < 10) {
             sec = '0' + sec;
         };
-        console.log(`${min}:${sec}.${ms}`);
     timeFace.textContent = `${min}:${sec}.${ms}`;
     }
 
-    function addLiToUl({ min, sec, ms }) {
-        laps.push(this.lapTime);
-        const li = document.createElement('li');
-        ul.append(li); 
-        if (min < 10) {
-            min = '0' + min;
-        };
-        if (sec < 10) {
-            sec = '0' + sec;
-        };
-        li.textContent = `${min}:${sec}.${ms}`;
+    function addLiToUl(addTime) {
+        if (!laps.includes(addTime)) {
+            laps.push(this.lapTime);
+            const li = document.createElement('li');
+            ul.append(li);
+            min = this.lapTime.min;
+            sec = this.lapTime.sec;
+            ms = this.lapTime.ms;
+            if (min < 10) {
+                min = '0' + min;
+            };
+            if (sec < 10) {
+                sec = '0' + sec;
+            };
+            li.textContent = `${min}:${sec}.${ms}`;
+            }   
     }
 
     function clearLaps() {
